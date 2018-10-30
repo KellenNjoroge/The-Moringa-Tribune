0d68c0d75cdc05f2fa2e508e592571858ceee034
KellenNjoroge <muthonkel@gmail.com>
2018-10-30 12:17:53 +0300

    added bootstrap

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
 
@@ -10,23 +10,14 @@ def welcome(request):
 
 def news_of_day(request):
     date = dt.date.today()
-    day = convert_dates()
-    html = f'''
-        <html>
-            <body>
-                <h1> News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-    '''
-    return HttpResponse(html)
 
+    return render(request, 'all-news/today-news.html',{'date': date})
 
-def convert_dates(dates):
-    day_number = dt.date.weekday(dates)
-    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday', 'Saturday', 'Sunday']
-    day = days[day_number]
-    return day
-
+# def convert_dates(dates):
+#     day_number = dt.date.weekday(dates)
+#     days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday','Friday', 'Saturday', 'Sunday']
+#     day = days[day_number]
+#     return day
 
 def past_days_news(request,past_date):
     try:
@@ -36,12 +27,7 @@ def past_days_news(request,past_date):
     except ValueError:
         raise Http404()
 
-        day = convert_dates(date)
-        html = f'''
-            <html>
-                <body>
-                    <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-                </body>
-            </html>
-                '''
-        return HttpResponse(html)
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-news/past-news.html', {"date": date})
