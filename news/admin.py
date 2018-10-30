580272f68d3ecc22f4ceff7ef51b74b9d644ec06
KellenNjoroge <muthonkel@gmail.com>
2018-10-30 16:02:17 +0300

    created first article

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . models import Editor,Article,Tags
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal = ('Tags')
+
+
+admin.site.register(Editor)
+admin.site.register(Article)
+admin.site.register(Tags)
