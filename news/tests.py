1681382aca5e6ca3dc7d9de3688b343dbf3c50d3
KellenNjoroge <muthonkel@gmail.com>
2018-10-30 15:26:41 +0300

    run tests

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Editors, Article,Tags
 
-# Create your tests here.
+class EditorsTestClass(TestCase):
+    #Set up method
+    def setUp(self):
+        self.james = Editors(first_name='Sly', last_name='keller', email='muthonkel@gmail.com')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.james, Editors))
